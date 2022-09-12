313aa155a4f5da76f21878da70373d71afb95837
WooJinDeve <jwj060111@gmail.com>
2022-09-12 15:57:27 +0900

    연관관계 매핑 예제

--- 자바 ORM 표준 JPA 프로그래밍/japshop/src/main/java/jpabook/japshop/domain/Order.java
+++ 자바 ORM 표준 JPA 프로그래밍/japshop/src/main/java/jpabook/japshop/domain/Order.java
@@ -2,7 +2,9 @@ package jpabook.japshop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -11,11 +13,23 @@ public class Order {
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -25,12 +39,20 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
