4193ab7288f53b1a113cb7f7ce080de7011d591f
WooJinDeve <jwj060111@gmail.com>
2022-09-13 15:05:07 +0900

    JPQL 기본문법

--- 자바 ORM 표준 JPA 프로그래밍/JPA 기본 설명/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍/JPA 기본 설명/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,10 @@
 package hellojpa;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class JpaMain {
 
@@ -12,32 +16,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("homeCity", "street", "address");
-            MemberV5 member = new MemberV5();
-            member.setUsername("member1");
-            member.setHomeAddress(address);
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity(new Address("old1", "street", "address")));
-            member.getAddressHistory().add(new AddressEntity(new Address("old2", "street", "address")));
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==============START===============");
-            MemberV5 findMember = em.find(MemberV5.class, member.getId());
-
-            //homecity -> newcity
-            Address newAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", newAddress.getStreet(), newAddress.getZipcode()));
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
+            List<Member> resultList = em.createNativeQuery("select * from MEMBER", Member.class)
+                    .getResultList();
 
 
             tx.commit();
